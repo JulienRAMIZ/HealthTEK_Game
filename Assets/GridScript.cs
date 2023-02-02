05be240e955e536be2ba2c5784e0da7d65dd8ef5
JulienRAMIZ <j.ramiz@estia.fr>
2023-02-02 13:31:09 +0100

    Update Grid script and linked it to Tile.cs

--- Assets/GridScript.cs
+++ Assets/GridScript.cs
@@ -2,9 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class GridScript : MonoBehaviour
 {
-    [SerializeField] private GameObject room;
+    [SerializeField] private Tile _tilePrefab;
     [SerializeField] private int _width, _height;
     [SerializeField] private Transform _cam;
 
@@ -15,11 +16,6 @@ public class GridScript : MonoBehaviour
         GenerateGrid();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void GenerateGrid()
     {
@@ -27,11 +23,17 @@ public class GridScript : MonoBehaviour
         {
             for (int y = 0; y < _height; y++)
             {
-                room.name = $"room {x} {y}";
-                Instantiate(room,new Vector3(x,y), Quaternion.identity);
-                
+                var spawnedTile = Instantiate(_tilePrefab, new Vector3(x, y), Quaternion.identity);
+                spawnedTile.name = $"room {x} {y}";
+
+
+                //var isOffset = (x % 2 == 0 && y % 2 != 0) || (x % 2 != 0 && y % 2 == 0);
+                var isOffset = (x % 2 != y % 2);
+                spawnedTile.Init(isOffset);
+
             }
         }
-        _cam.transform.position = new Vector3(_width / 2 -0.5f, _height / 2 -0.5f, -10);
+        _cam.transform.position = new Vector3((float)_width / 2 - 0.5f, (float)_height / 2 - 0.5f, -10);
+
     }
 }
