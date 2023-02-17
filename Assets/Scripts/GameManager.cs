e4f198164814315b826ecd6a9d12ee45af065048
Chelza <c.inzouddine@estia.fr>
2023-02-17 17:10:31 +0100

    Managed "Next" and "Previous" Buttons, created the code that retrieves a Q&A csv file and generates random questions from it, created the code that compares the answer choice of the player and verifies it is correct or not

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,36 +1,145 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System;
 using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject titleScreen;
-    public TextMeshProUGUI timeText;
+    // public TextMeshProUGUI TimeText;
+    // public TextMeshProUGUI QuestionTitle;
+    // private float time = 0.0f; //timer
 
-    private float time = 0.0f; //timer
+    public GameObject TitleScreen;
+    public TextMeshProUGUI QuestionText;
+    public Button QuestionButton;
+    public GameObject QuestionScreen;
+    public GameObject[] Choices;
+    public List<string> QnA;
+    private string CorrectAnswer;
+    private GameObject SelectedButton;
+    private bool CorrectChoice = false;
+    private readonly string FilePath = "C:/DEV/QnA.csv";
+    private int RandomIndex;
+
+    void Start()
+    {
+        QuestionScreen.SetActive(false);
+
+        //Open the questions and answers file and retrieve the values
+        var reader = new StreamReader(File.OpenRead(FilePath));
+        while (!reader.EndOfStream)
+        {
+            var line = reader.ReadLine();
+            var values = line.Split(';');
+            QnA.Add(values[0]);
+            QnA.Add(values[1]);
+            QnA.Add(values[2]);
+        }
+        QnA.RemoveRange(0, 3); //remove first values since they correspond to the first row of the CSV file (Question, Possible_Answer, Correct_Answer)
+    }
+
     // Start the game, remove title screen, reset timer, and adjust the questions based on difficulty button clicked
     public void StartGame(int difficulty)
     {
-        // Voir comment ajuster la difficulté (question difficle, labyrinthe plus compliqué) 
-        titleScreen.SetActive(false);
-        
+        //Voir comment ajuster la difficulté (question difficle, labyrinthe plus compliqué) 
+        TitleScreen.SetActive(false); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        UpdateTime(); // faire en sorte que le timer commence après avoir cliqué sur le bouton
+       // UpdateTime(); // faire en sorte que le timer commence après avoir cliqué sur le bouton
     }
 
-    public void UpdateTime()
+    /*public void UpdateTime()
     {
         time += Time.deltaTime;
-        timeText.text = "Timer: " + Mathf.Round(time); //refaire timer correctement, ainsi il y aurait bien heures, minutes et secondes
+        TimeText.text = "Timer: " + Mathf.Round(time); //refaire timer correctement, ainsi il y aurait bien heures, minutes et secondes
+    }*/
+
+    public void PopUpQuestion() 
+    {
+        QuestionScreen.SetActive(true);
+        QuestionButton.gameObject.SetActive(false);
+        RandomIndex = UnityEngine.Random.Range(0, QnA.Count);
+        QuestionText.text = QnA[RandomIndex];
+
+        while (!QuestionText.text.EndsWith('?'))
+        {
+            RandomIndex = UnityEngine.Random.Range(0, QnA.Count);
+            QuestionText.text = QnA[RandomIndex];
+        }
+
+        SetAnswers();
     }
 
+    // Set the possible answers from the csv file to the choice buttons on Unity
+    public void SetAnswers() 
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            Choices[j].transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = (QnA[RandomIndex + 1].Split(','))[j];
+        }
+        CorrectAnswer = QnA[RandomIndex + 2];
+        Debug.Log(CorrectAnswer);
+    }
+
+    //Check if the player chose the correct answer
+    public void IsCorrect()
+    {
+        CheckButton(Choices);
+    }
+    public void CheckButton(GameObject[] Choices)
+    {
+        SelectedButton = EventSystem.current.currentSelectedGameObject;
+        Debug.Log(SelectedButton);
+        for (int i = 0; i < Choices.Length; i++)
+        {
+            if (SelectedButton == Choices[i])
+            {
+                if (Choices[i].transform.GetChild(0).GetComponent<TextMeshProUGUI>().text == CorrectAnswer)
+                {
+                    Debug.Log("Correct Answer");
+                    CorrectChoice = true;
+                    break;
+                }
+                else
+                {
+                    Debug.Log("Wrong Answer");
+                    CorrectChoice = false;
+                    //PUNITION POSSIBILITE DE RECOMMENCER DANS X MINUTES
+                }
+            }
+        }
+    }
+    public void CheckCorrect()
+    {
+        if (CorrectChoice)
+        {
+            QuestionScreen.SetActive(false);
+            QuestionButton.gameObject.SetActive(true);
+        }
+    }
+    
+   /* public void CheckCorrect(Button button)
+    {
+       if( button.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text == CorrectAnswer)
+       {
+            Debug.Log("Correct Answer");
+            QuestionScreen.SetActive(false);
+            QuestionButton.gameObject.SetActive(true);
+       }
+       else 
+       {
+            Debug.Log("Wrong Answer");
+       }
+    }*/
+    
     /* Restart game by reloading the scene
      * 
     public void RestartGame()
