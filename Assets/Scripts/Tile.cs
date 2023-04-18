87d45227b70537b23f6262adae66b1599d5b9906
JulienRAMIZ <j.ramiz@estia.fr>
2023-04-18 09:46:44 +0200

    Using new Input System, previous functions were not compatbile Add a popUpQuestion when we click on a closed room (redlight)

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -1,21 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Tile : MonoBehaviour
+public class Tile : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler
 {
     [SerializeField] public GameObject _room;
     [SerializeField] private Color _baseColor, _offsetColor;
     [SerializeField] private SpriteRenderer _renderer;
-    [SerializeField] private GameObject _highlight;
+    [SerializeField] private GameObject _whiteHighlight;
+    [SerializeField] private GameObject _redHighlight;
     
-    private PlayerController player;
+    public PlayerController player;
     private GridScript grid;
+    private GameManager manager;
+    public bool goQnA;
+    public bool isClose = false;
+
 
 
     public void Start()
     {
         player = GameObject.Find("Character").GetComponent<PlayerController>();
+        manager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
     public void Init(bool isOffset)
     {
@@ -23,27 +31,46 @@ public class Tile : MonoBehaviour
         _renderer.color = isOffset ? _offsetColor : _baseColor;
     }
 
-    void OnMouseEnter()
+    void IPointerEnterHandler.OnPointerEnter(PointerEventData eventData)
     {
-        _highlight.SetActive(true);
+        if (player.transform.position.x - this.transform.position.x == MathF.Abs(1) && player.transform.position.y - this.transform.position.y == MathF.Abs(1))
+        {
+            isClose = true;
+        }
 
-        if (this.CompareTag("OpenedDoor") == true)
-        {   
-            Debug.Log("test");
-            player.ableMoving = true;
+        if (CompareTag("OpenedDoor") == true)
+        {
+            _whiteHighlight.SetActive(true);
+            if (isClose) { player.ableMoving = true; }
         }
         else
         {
-            //player.ableMoving = false;
+            _redHighlight.SetActive(true);
+            player.ableMoving = false;
+
         }
+
     }
 
-    void OnMouseExit()
+
+    void IPointerExitHandler.OnPointerExit(PointerEventData eventData)
     {
-        _highlight.SetActive(false);
-        if (player.isMoving == false)
+        _whiteHighlight.SetActive(false);
+        if (goQnA == false) 
+        { 
+            _redHighlight.SetActive(false); 
+        
+        }
+        isClose = false;
+
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Left && _redHighlight == true)
         {
-            player.ableMoving = false;
+            goQnA = true;
+            manager.PopUpQuestion();
         }
 
     }
