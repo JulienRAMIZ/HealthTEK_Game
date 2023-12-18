00ac144a2b926ab6a5747668fc3fdadf53722283
JulienRAMIZ <j.ramiz@estia.fr>
2023-12-18 16:37:47 +0100

    Jolanta version: add possibility to select quizz ?

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -67,7 +67,7 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         //var QuizFile = Resources.Load<TextAsset>("/QnA_Files/QnA");
-        var QuizFile = Path.Combine(Application.streamingAssetsPath,"QnA_Files/QnA_michel.csv");
+        var QuizFile = Path.Combine(Application.streamingAssetsPath,"QnA_Files/QnA_biomechanics.csv");
         string FilePath = QuizFile;
 
         Debug.Log(FilePath);
